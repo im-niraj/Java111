96a261a41b4a5722406841de4abeed0438ac192c
Niraj Kumar <n1r4jkumar@gmail.com>
2022-06-02 21:31:08 +0530

    question no 2 updated and changed some methods

--- Assignment_Day11/src/question2/Mobile.java
+++ Assignment_Day11/src/question2/Mobile.java
@@ -2,36 +2,33 @@ package question2;
 
 public class Mobile {
 	String[] outdatedModels = {"note4","note5","note6","note7"};
-	
-	public void searchOutdatedModel(String model) {
-		Boolean flag = true;
-		for(String xString : outdatedModels) {
-			if(xString == model) {
-				System.out.println(xString+"_OUTDATED");
-				flag = false;
+
+	public void searchOutdatedModel(String brand, String... model) {
+		if(model.length>0) {
+			for(String xString : model) {
+				Boolean flag = true;
+				for(String modelName : outdatedModels) {
+					if(xString.equals(modelName)){
+						System.out.println(xString+"_OUTDATED");
+						flag = false;
+					}
+					
+				}
+				if(flag) {
+					System.out.println(brand+"_"+xString+"_New");
+				}
 			}
 		}
-		if(flag) {
-			System.out.println(model);
+		else {
+			System.out.println(brand+": model no is missing");
 		}
-	}
-	public void searchOutdatedModel(String brand, String model) {
-		Boolean flag = true;
-		for(String xString : outdatedModels) {
-			if(xString == model) {
-				System.out.println(brand+"_"+xString+"_OUTDATED");
-				flag = false;
-			}
-		}
-		if(flag) {
-			System.out.println(brand+"_"+model);
-		}
+		
 	}
 	
 	public static void main(String[] args) {
 		Mobile mobile = new Mobile();
-		mobile.searchOutdatedModel("note7");
+		mobile.searchOutdatedModel("Samsung");
 		mobile.searchOutdatedModel("Samsung","note7");
-		mobile.searchOutdatedModel("Samsung","note8");
+		mobile.searchOutdatedModel("Samsung","note7","note4","note5","note10");
 	}
 }
